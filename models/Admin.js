cae5aa63803fbe0f3d71de962adce9516d9a6524
sammylwolfson <samuel.levi.wolfson@gmail.com>
2022-06-06 20:52:09 -0700

    added admin login

---
+++ models/Admin.js
@@ -0,0 +1,47 @@
+const {Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection:');
+
+class Admin extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+Admin.init ({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [5]
+        }
+    }
+
+}, 
+{
+    hooks: {
+        async beforeCreate(newUserData){
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeUpdate(updatedUserData){
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+
+    }, 
+    sequelize, 
+    timestamps: false,
+    freezeTableName: true,
+    modelName: 'admin'
+});
