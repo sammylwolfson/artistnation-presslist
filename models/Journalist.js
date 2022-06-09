d5f50161ea353e30e3eb1aaa97e0165b1a304ff0
Akeva Melchor <akeva.melchor@expel.io>
2022-06-08 19:17:07 -0700

    add city model

--- models/Journalist.js
+++ models/Journalist.js
@@ -3,43 +3,44 @@ const { Model, DataTypes, Sequelize } = require("sequelize");
 
 class Journalist extends Model {}
 
-Journalist.init(
-  {
+Journalist.init({
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     first_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     last_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     company: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    city: {
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
     },
-  },
-  {
+}, {
     underscored: true,
     sequelize,
     timestamps: true,
     freezeTableName: true,
     underscored: true,
     modelName: "journalist",
-  }
-);
+});
 
 module.exports = Journalist;
