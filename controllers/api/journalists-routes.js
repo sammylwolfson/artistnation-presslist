a6e1119223ceaf34abe83187f8f5981d92b4a508
Debbie Neflas <debbieneflas@gmail.com>
2022-06-08 21:39:50 -0700

    update journalist search route

--- controllers/api/journalists-routes.js
+++ controllers/api/journalists-routes.js
@@ -51,57 +51,54 @@ router.get("/:id", withAuth, (req, res) => {
 });
 
 // get journalists by search for: first name, last name, city, email, company, date added
-router.get(
-  "/:first_name?/:last_name?/:city?/:email?/:company?/:date_added?",
-  withAuth,
-  (req, res) => {
-    // logic to omit from where if null
-    let params = {};
-    //if parameter is not null, add property to params object (may need to be !=" ")
-    if (req.params.first_name) {
-      params.first_name = req.params.first_name;
-    }
-    if (req.params.last_name) {
-      params.last_name = req.params.last_name;
-    }
-    if (req.params.city) {
-      params.city = req.params.city;
-    }
-    if (req.params.email) {
-      params.email = req.params.email;
-    }
-    if (req.params.company) {
-      params.company = req.params.company;
-    }
-    if (req.params.date_added) {
-      params.date_added = req.params.date_added;
-    }
-    Journalist.findAll({
-      where: params,
-      attributes: [
-        "id",
-        "first_name",
-        "last_name",
-        "email",
-        "city",
-        "created_at",
-      ],
-    })
-      .then((dbJournalistData) => {
-        if (!dbJournalistData) {
-          res
-            .status(404)
-            .json({ message: "No journalist found matching this criteria" });
-          return;
-        }
-        res.status(200).json(dbJournalistData);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+router.post("/search", withAuth, (req, res) => {
+  // logic to omit from where if null
+  let body = {};
+  //if parameter is not null, add property to body object (may need to be !=" ")
+  if (req.body.first_name) {
+    body.first_name = req.body.first_name;
   }
-);
+  if (req.body.last_name) {
+    body.last_name = req.body.last_name;
+  }
+  if (req.body.city) {
+    body.city = req.body.city;
+  }
+  if (req.body.email) {
+    body.email = req.body.email;
+  }
+  if (req.body.company) {
+    body.company = req.body.company;
+  }
+  if (req.body.date_added) {
+    body.date_added = req.body.date_added;
+  }
+  Journalist.findAll({
+    where: body,
+    attributes: [
+      "id",
+      "first_name",
+      "last_name",
+      "email",
+      "city",
+      "company",
+      "created_at",
+    ],
+  })
+    .then((dbJournalistData) => {
+      if (!dbJournalistData) {
+        res
+          .status(404)
+          .json({ message: "No journalist found matching this criteria" });
+        return;
+      }
+      res.status(200).json(dbJournalistData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // create new journalist
 router.post("/", withAuth, (req, res) => {
