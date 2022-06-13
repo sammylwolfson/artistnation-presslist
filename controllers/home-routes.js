97ff366738e7e98e6a74aa40614990508d53a92c
Debbie Neflas <debbieneflas@gmail.com>
2022-06-13 10:46:29 -0700

    navbar and logout javascript

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,9 @@ router.get("/", (req, res) => {
     res.redirect("/login");
     return;
   }
-  res.render("homepage");
+  res.render("homepage", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 
 router.get("/login", (req, res) => {
@@ -34,6 +36,57 @@ router.get("/journalists", (req, res) => {
       const journalists = dbJournalistData.map((journalist) =>
         journalist.get({ plain: true })
       );
+      res.render("journalists", {
+        journalists,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/search", (req, res) => {
+  let body = {};
+  //if parameter is not null, add property to body object (may need to be !=" ")
+  if (req.body.first_name) {
+    body.first_name = req.body.first_name;
+  }
+  if (req.body.last_name) {
+    body.last_name = req.body.last_name;
+  }
+  if (req.body.city) {
+    body.city = req.body.city;
+  }
+  if (req.body.email) {
+    body.email = req.body.email;
+  }
+  if (req.body.company) {
+    body.company = req.body.company;
+  }
+  if (req.body.date_added) {
+    body.date_added = req.body.date_added;
+  }
+  console.log("body", body);
+  Journalist.findAll({
+    where: body,
+    attributes: [
+      "id",
+      "first_name",
+      "last_name",
+      "email",
+      "city",
+      "company",
+      "created_at",
+    ],
+  })
+    .then((dbJournalistData) => {
+      const journalists = dbJournalistData.map((journalist) =>
+        journalist.get({ plain: true })
+      );
+
+      console.log("response", journalists);
       res.render("journalists", {
         journalists,
         loggedIn: req.session.loggedIn,
--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,11 +1,11 @@
 async function searchFormHandler(event) {
   event.preventDefault();
 
-  const first_name = document.querySelector("#add-first-name").value.trim();
-  const last_name = document.querySelector("#add-last-name").value.trim();
-  const email = document.querySelector("#add-email").value.trim();
-  const company = document.querySelector("#add-company").value.trim();
-  const city = document.querySelector("#add-city").value.trim();
+  const first_name = document.querySelector("#search-first-name").value.trim();
+  const last_name = document.querySelector("#search-last-name").value.trim();
+  const email = document.querySelector("#search-email").value.trim();
+  const company = document.querySelector("#search-company").value.trim();
+  const city = document.querySelector("#search-city").value.trim();
 
   const response = await fetch("api/journalists/search", {
     method: "POST",
@@ -23,7 +23,7 @@ async function searchFormHandler(event) {
 
   if (response.ok) {
     // verify the route to all journalists------------------------------------------!!!!!!!!!!!!!
-    document.location.replace("/journalists");
+    // document.location.replace("/journalists");
   } else {
     alert(response.statusText);
   }
