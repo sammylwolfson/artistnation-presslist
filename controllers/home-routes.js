e5fa432ecd6cb41b8105c2646eab60859086a9c2
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-06-13 19:15:05 -0700

    removed bad code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -114,40 +114,5 @@ router.get('/journalist/:id', (req, res)=>{
 });
 
 
-router.get('/search/:id', (req, res)=>{
-  console.log(req.params.id.split(','))
-  Journalist.findAll({
-    where: {
-      id: req.params.id.split(',')
-    },
-    attributes: [
-      "id",
-      "first_name",
-      "last_name",
-      "company",
-      "city",
-      "created_at",
-      "email",
-    ]
-  })
-  .then(dbJournalistData=>{
-    if(!dbJournalistData){
-      res.status(404).json({ message: 'No journalist found with that id' });
-      return;
-    }
-    const journalists = dbJournalistData.map((journalist) => journalist.get({ plain: true }));
-    res.render(
-      'journalists',
-       {
-        journalists,
-        loggedIn: true
-      })
-  })
-  .catch(err=>{
-    console.log(err);
-    res.status(500).json(err);
-  });
-});
-
 
 module.exports = router;
