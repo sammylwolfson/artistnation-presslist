541f901fc540d701de654cba4a7d5dc5440ebd08
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-06-15 15:25:38 -0700

    exclude passwords

--- controllers/api/admin-routes.js
+++ controllers/api/admin-routes.js
@@ -7,6 +7,7 @@ const withAuth = require("../../utils/auth");
 //get all admin
 router.get("/", (req, res) => {
   Admin.findAll({
+    attributes: { exclude: ["password"] },
   })
     .then((dbAdminData) => res.status(200).json(dbAdminData))
     .catch((err) => {
@@ -18,6 +19,7 @@ router.get("/", (req, res) => {
 // get admin by id
 router.get("/:id", (req, res) => {
   Admin.findOne({
+    attributes: { exclude: ["password"] },
     where: {
       id: req.params.id,
     },
