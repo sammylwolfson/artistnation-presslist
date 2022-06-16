4af2e448b6aaa1acd37b3dc8bb6dee35869ce3cd
Debbie Neflas <debbieneflas@gmail.com>
2022-06-15 20:19:38 -0700

    email all feature, and sorting feature

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
+const sortRoutes = require("./sort-routes");
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
+router.use("/sort", sortRoutes);
 
 // if a request is made to endpoint that doesn't exist, a 404 error will occur
 router.use((req, res) => {
